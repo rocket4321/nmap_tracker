557a558ca174e73f8a457b0c7e1c776cae3a9b41
rocket4321 <77749413+rocket4321@users.noreply.github.com>
2021-03-14 23:10:55 -0700

    consider_home

--- nmap_tracker/const.py
+++ nmap_tracker/const.py
@@ -1,17 +1,17 @@
 """Constants for the Nmap Tracker component."""
 
-ATTR_DURATION = "duration_sec"
-ATTR_PROCESSOR = "processor"
 ATTR_RESPONSE_REASON = "response_reason"
 
 CONF_DEBUG_LEVEL = "debug_log_level"
 CONF_EXCLUDE = "exclude"
 CONF_EXCLUDE_ACTIVE = "exclude_active"
 CONF_EXCLUDE_MAC = "exclude_mac"
+CONF_EXCLUSIVE_MAC = "exclusive_mac"
 
 # Interval in minutes to exclude devices from a scan while they are home
 CONF_HOME_INTERVAL = "home_interval"
 CONF_INCLUDE_NO_MAC = "include_no_mac"
+
 CONF_OPTIONS = "scan_options"
 
 # Timeout in seconds before nmap quits, default is 60 seconds
@@ -22,19 +22,14 @@ DEFAULT_EXCLUDE_ACTIVE = True
 DEFAULT_INCLUDE_NO_MAC = False
 DEFAULT_OPTIONS = "-F --host-timeout 5s"
 DEFAULT_LOCAL_MAC_NAME = "localhost"
-DEFAULT_PROCESS_EVAL_INTERVAL = 5
-#DEFAULT_TIMEOUT = "10"
-DEFAULT_TIMEOUT = "60"
+DEFAULT_TIMEOUT = "11"
 
 # Debug log verbosity
 # WARNING: Level 3+ includes MAC addresses in logs
-DEFAULT_DEBUG_LEVEL = "1"
+DEFAULT_DEBUG_LEVEL = "2"
 
 # MAC utilized when none provided by nmap results scan (local device)
 DEFAULT_MAC = "XX:XX:XX:XX:XX:XX"
-
-# Provides Info level message to user if duration is exceeded
-NMAP_DURATION_MAX_MSG = 10
 
 NMAP_STATUS_REASON_LOCAL = "localhost-response"
 
--- nmap_tracker/device_tracker.py
+++ nmap_tracker/device_tracker.py
@@ -12,22 +12,29 @@ from getmac import get_mac_address
 from nmap import PortScanner, PortScannerError, PortScannerTimeout
 
 from homeassistant.components.device_tracker import (
-    DOMAIN,
-    PLATFORM_SCHEMA,
     DeviceScanner,
+    PLATFORM_SCHEMA,
 )
 
-from homeassistant.const import CONF_HOSTS, CONF_SCAN_INTERVAL
+from homeassistant.components.device_tracker.const import (
+    CONF_CONSIDER_HOME,
+    CONF_SCAN_INTERVAL,
+    DEFAULT_CONSIDER_HOME,
+    DOMAIN,
+    SCAN_INTERVAL,
+)
+
+from homeassistant.const import CONF_HOSTS
 import homeassistant.helpers.config_validation as cv
 import homeassistant.util.dt as dt_util
 
 from .const import (
-    ATTR_DURATION,
     ATTR_RESPONSE_REASON,
     CONF_DEBUG_LEVEL,
     CONF_EXCLUDE,
     CONF_EXCLUDE_ACTIVE,
     CONF_EXCLUDE_MAC,
+    CONF_EXCLUSIVE_MAC,
     CONF_HOME_INTERVAL,
     CONF_INCLUDE_NO_MAC,
     CONF_OPTIONS,
@@ -40,7 +47,6 @@ from .const import (
     DEFAULT_MAC,
     DEFAULT_OPTIONS,
     DEFAULT_TIMEOUT,
-    NMAP_DURATION_MAX_MSG,
     NMAP_STATUS_REASON_LOCAL,
 )
 
@@ -50,10 +56,10 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_HOSTS): cv.ensure_list,
-        vol.Required(CONF_HOME_INTERVAL, default=0): cv.positive_int,
         vol.Optional(CONF_EXCLUDE, default=[]): vol.All(cv.ensure_list, [cv.string]),
         vol.Optional(CONF_EXCLUDE_ACTIVE, default=DEFAULT_EXCLUDE_ACTIVE): cv.boolean,
         vol.Optional(CONF_EXCLUDE_MAC, default=[]): vol.All(cv.ensure_list, [cv.string]),
+        vol.Optional(CONF_EXCLUSIVE_MAC, default=[]): vol.All(cv.ensure_list, [cv.string]),
         vol.Optional(CONF_OPTIONS, default=DEFAULT_OPTIONS): cv.string,
         vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
         vol.Optional(CONF_DEBUG_LEVEL, default=DEFAULT_DEBUG_LEVEL): cv.positive_int,
@@ -78,7 +84,6 @@ class NmapDeviceScanner(DeviceScanner):
 
     def __init__(self, config):
         """Initialize the scanner."""
-        self.duration = None
         self.command_line = []
         self.last_results = []
         self.hosts = config[CONF_HOSTS]
@@ -86,10 +91,11 @@ class NmapDeviceScanner(DeviceScanner):
         self.exclude_active = config[CONF_EXCLUDE_ACTIVE]
         exclude_mac = config[CONF_EXCLUDE_MAC]
         self.exclude_mac = [mac.upper() for mac in exclude_mac]
-        home_interval_minutes = config[CONF_HOME_INTERVAL]
+        exclusive_mac = config[CONF_EXCLUSIVE_MAC]
+        self.exclusive_mac = [mac.upper() for mac in exclusive_mac]
+        self.home_interval = config[CONF_CONSIDER_HOME]
         self.timeout = timedelta(seconds=config[CONF_TIMEOUT])
         self._options = config[CONF_OPTIONS]
-        self.home_interval = timedelta(minutes=home_interval_minutes)
         self.local_mac_name = config[CONF_LOCAL_MAC_NAME]
         self.debug_level = int(config[CONF_DEBUG_LEVEL])
         self.include_no_mac = config[CONF_INCLUDE_NO_MAC]
@@ -100,14 +106,16 @@ class NmapDeviceScanner(DeviceScanner):
             scan_interval = config[CONF_SCAN_INTERVAL]
             scan_interval_sec = scan_interval.total_seconds()
         except:
-# FIXME
-# Ask HASS chat
-# Can't locate default scan_interval in core constants        
-            scan_interval_sec = 600            
-        if (home_interval_minutes * 60) <= scan_interval_sec:
-            _LOGGER.warning("Value set for '%s'='%s' conflicts with '%s'='%s'. Home interval (minutes) should exceed scan interval (seconds).",
-                 CONF_HOME_INTERVAL, self.home_interval, CONF_SCAN_INTERVAL, scan_interval_sec)
-# How to determine scan interval        
+            scan_interval = SCAN_INTERVAL
+            scan_interval_sec = scan_interval.total_seconds()
+        # Confirm scan interval is shorter than home interval                
+        if self.home_interval.total_seconds() <= scan_interval_sec:
+            _LOGGER.warning("Value set for '%s'='%s' conflicts with '%s'='%s'. Consider home (seconds) should exceed scan interval (seconds).",
+                 CONF_CONSIDER_HOME, self.home_interval.total_seconds(), CONF_SCAN_INTERVAL, scan_interval_sec)
+        # Confirm scan interval is longer than timeout
+        if (config[CONF_TIMEOUT]) >= scan_interval_sec:
+            _LOGGER.warning("Value set for '%s'='%s' conflicts with '%s'='%s'. Scan interval (seconds) should exceed timeout (seconds).",
+                 CONF_TIMEOUT, config[CONF_TIMEOUT], CONF_SCAN_INTERVAL, scan_interval_sec)
         _LOGGER.debug("Scanner initialized")
        
     def scan_devices(self):
@@ -116,6 +124,8 @@ class NmapDeviceScanner(DeviceScanner):
         if self.debug_level >= 4:
             _LOGGER.debug("Nmap last results for %s = %s"
                 , ','.join(self.hosts), self.last_results)
+        if len(self.exclusive_mac) > 0:
+            return [device.mac for device in self.last_results if device.mac in self.exclusive_mac]
         if self.include_no_mac:
             return [device.mac for device in self.last_results]
         else:
@@ -162,14 +172,12 @@ class NmapDeviceScanner(DeviceScanner):
         return {
 		    "ip": filter_ip,
 		    "mac": device,
-            ATTR_DURATION: self.duration,
             ATTR_RESPONSE_REASON: filter_reason,
 	    }
 
     def _update_info(self):
         """Scan the network for devices.
         """
-        command_line = []
         options = self._options
         if self.home_interval:
             boundary = dt_util.now() - self.home_interval
@@ -185,12 +193,14 @@ class NmapDeviceScanner(DeviceScanner):
             exclude_hosts = self.exclude
         if exclude_hosts:
             options += f" --exclude {','.join(exclude_hosts)}"
-
+       
         # start a thread to start nmap and process results
-        self._process_host(last_results
-                        , command_line
+        self.last_results = last_results
+        self._process_host(self.last_results
+                        , self.command_line
                         , self.hosts
                         , exclude_hosts
+                        , self.exclusive_mac
                         , options
                         , self.timeout
                         , self.exclude_mac
@@ -204,6 +214,7 @@ class NmapDeviceScanner(DeviceScanner):
                         , command_line
                         , hosts
                         , exclude_hosts
+                        , exclusive_mac
                         , options
                         , timeout
                         , exclude_mac
@@ -214,12 +225,12 @@ class NmapDeviceScanner(DeviceScanner):
         """
         # start a thread to start nmap and process results
         start_time = dt_util.now()
-        processor_thread = None
         processor = NmapProcessor(
 		    last_results
 			, command_line
 			, hosts
 			, exclude_hosts
+			, exclusive_mac
 			, options
 			, timeout
 			, exclude_mac
@@ -229,16 +240,6 @@ class NmapDeviceScanner(DeviceScanner):
 		)
         processor.start()
 
-        # Sleep and wait until nmap results return/processed
-        while (processor.is_alive()):
-                time.sleep(1)               
-        self.last_results = last_results
-        self.command_line = command_line
-        self.duration = (dt_util.now() - start_time).total_seconds()
-
-        if self.duration >= NMAP_DURATION_MAX_MSG:
-            _LOGGER.info("Prolonged scan duration at %.1f seconds for %s : Command ='%s'"
-		, self.duration, ','.join(hosts), command_line)
 
 class NmapProcessor(threading.Thread):
     """This class scans for devices using nmap."""
@@ -248,6 +249,7 @@ class NmapProcessor(threading.Thread):
 		, command_line
 		, hosts
 		, exclude_hosts
+		, exclusive_mac
 		, options
 		, timeout
 		, exclude_mac
@@ -262,6 +264,7 @@ class NmapProcessor(threading.Thread):
         self._command_line = command_line
         self._hosts = hosts
         self._exclude_hosts = exclude_hosts
+        self._exclusive_mac = exclusive_mac
         self._options = options
         self._timeout = timeout
         self._exclude_mac = exclude_mac
@@ -279,12 +282,16 @@ class NmapProcessor(threading.Thread):
         now = dt_util.now()
         for ipv4, info in result["scan"].items():
             if self._debug_level >= 3:
-                _LOGGER.debug("Processing %s", ipv4)
+                _LOGGER.debug("Processing %s %s", ipv4, info["status"]["state"])
             if info["status"]["state"] != "up":
                 continue
             name = info["hostnames"][0]["name"] if info["hostnames"] else ipv4
             # Mac address only returned if nmap ran as root
             mac = info["addresses"].get("mac") or get_mac_address(ip=ipv4)
+            if len(self._exclusive_mac) > 0 and mac not in self._exclusive_mac:
+                if self._debug_level >= 4:
+                    _LOGGER.debug("Ignoring (reason: not in exclusive mac list) %s %s", ipv4, mac)
+                continue
             reason = info["status"]["reason"]
             if mac is None:
                 # nmap will not report MAC for local ip, so ignore for single case
@@ -313,10 +320,10 @@ class NmapProcessor(threading.Thread):
 			, timeout=self._timeout.total_seconds()
 			)
         except PortScannerTimeout:
-            _LOGGER.warning("Nmap Timeout for %s", self._hosts)
+            _LOGGER.info("Nmap Timeout for %s", self._hosts)
             return
-        except PortScannerError:
-            _LOGGER.info("Nmap Exception for %s :", self._hosts)
+        except PortScannerError as e:
+            _LOGGER.info("Nmap Exception for %s : %s", ','.join(self._hosts), str(e))
             return
         if self._debug_level >= 1:
             _LOGGER.debug("Nmap Command: %s", self._scanner.command_line())
